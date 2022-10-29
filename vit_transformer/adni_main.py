863425ca928b8d0428da351b3eb1666f7595dad1
Puneet Bawa <erpuneetbawa@gmail.com>
2022-10-29 11:20:09 +0530

    Create adni_main.py

---
+++ vit_transformer/adni_main.py
@@ -0,0 +1,144 @@
+import torchvision
+from torchvision.transforms import ToTensor
+
+train_ds = torchvision.datasets.ImageFolder('/content/train/', transform=ToTensor())
+valid_ds = torchvision.datasets.ImageFolder('/content/valid/', transform=ToTensor())
+test_ds = torchvision.datasets.ImageFolder('/content/test/', transform=ToTensor())
+
+from transformers import ViTModel
+from transformers.modeling_outputs import SequenceClassifierOutput
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ViTForImageClassification(nn.Module):
+    def __init__(self, num_labels=3):
+        super(ViTForImageClassification, self).__init__()
+        self.vit = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
+        self.dropout = nn.Dropout(0.1)
+        self.classifier = nn.Linear(self.vit.config.hidden_size, num_labels)
+        self.num_labels = num_labels
+
+    def forward(self, pixel_values, labels):
+        outputs = self.vit(pixel_values=pixel_values)
+        output = self.dropout(outputs.last_hidden_state[:,0])
+        logits = self.classifier(output)
+
+        loss = None
+        if labels is not None:
+          loss_fct = nn.CrossEntropyLoss()
+          loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+        if loss is not None:
+          return logits, loss.item()
+        else:
+          return logits, None
+
+EPOCHS = 3
+BATCH_SIZE = 10
+LEARNING_RATE = 2e-5
+
+from transformers import ViTFeatureExtractor
+import torch.nn as nn
+import torch
+# Define Model
+model = ViTForImageClassification(len(train_ds.classes))    
+# Feature Extractor
+feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224-in21k')
+# Adam Optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+# Cross Entropy Loss
+loss_func = nn.CrossEntropyLoss()
+# Use GPU if available  
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') 
+if torch.cuda.is_available():
+    model.cuda() 
+    
+import torch.utils.data as data
+from torch.autograd import Variable
+import numpy as np
+
+print("Number of train samples: ", len(train_ds))
+print("Number of test samples: ", len(test_ds))
+print("Detected Classes are: ", train_ds.class_to_idx) 
+
+train_loader = data.DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True,  num_workers=4)
+test_loader  = data.DataLoader(test_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=4) 
+
+# Train the model
+for epoch in range(EPOCHS):        
+  for step, (x, y) in enumerate(train_loader):
+    # Change input array into list with each batch being one element
+    x = np.split(np.squeeze(np.array(x)), BATCH_SIZE)
+    # Remove unecessary dimension
+    for index, array in enumerate(x):
+      x[index] = np.squeeze(array)
+    # Apply feature extractor, stack back into 1 tensor and then convert to tensor
+    x = torch.tensor(np.stack(feature_extractor(x)['pixel_values'], axis=0))
+    # Send to GPU if available
+    x, y  = x.to(device), y.to(device)
+    b_x = Variable(x)   # batch x (image)
+    b_y = Variable(y)   # batch y (target)
+    # Feed through model
+    output, loss = model(b_x, None)
+    # Calculate loss
+    if loss is None: 
+      loss = loss_func(output, b_y)   
+      optimizer.zero_grad()           
+      loss.backward()                 
+      optimizer.step()
+
+    if step % 50 == 0:
+      # Get the next batch for testing purposes
+      test = next(iter(test_loader))
+      test_x = test[0]
+      # Reshape and get feature matrices as needed
+      test_x = np.split(np.squeeze(np.array(test_x)), BATCH_SIZE)
+      for index, array in enumerate(test_x):
+        test_x[index] = np.squeeze(array)
+      test_x = torch.tensor(np.stack(feature_extractor(test_x)['pixel_values'], axis=0))
+      # Send to appropirate computing device
+      test_x = test_x.to(device)
+      test_y = test[1].to(device)
+      # Get output (+ respective class) and compare to target
+      test_output, loss = model(test_x, test_y)
+      test_output = test_output.argmax(1)
+      # Calculate Accuracy
+      accuracy = (test_output == test_y).sum().item() / BATCH_SIZE
+      print('Epoch: ', epoch, '| train loss: %.4f' % loss, '| test accuracy: %.2f' % accuracy)
+  import matplotlib.pyplot as plt
+import numpy as np
+
+EVAL_BATCH = 1
+eval_loader  = data.DataLoader(valid_ds, batch_size=EVAL_BATCH, shuffle=True, num_workers=4) 
+# Disable grad
+with torch.no_grad():
+    
+  inputs, target = next(iter(eval_loader))
+  # Reshape and get feature matrices as needed
+  print(inputs.shape)
+  inputs = inputs[0].permute(1, 2, 0)
+  # Save original Input
+  originalInput = inputs
+  for index, array in enumerate(inputs):
+    inputs[index] = np.squeeze(array)
+  inputs = torch.tensor(np.stack(feature_extractor(inputs)['pixel_values'], axis=0))
+
+  # Send to appropriate computing device
+  inputs = inputs.to(device)
+  target = target.to(device)
+ 
+  # Generate prediction
+  prediction, loss = model(inputs, target)
+    
+  # Predicted class value using argmax
+  predicted_class = np.argmax(prediction.cpu())
+  value_predicted = list(valid_ds.class_to_idx.keys())[list(valid_ds.class_to_idx.values()).index(predicted_class)]
+  value_target = list(valid_ds.class_to_idx.keys())[list(valid_ds.class_to_idx.values()).index(target)]
+        
+  # Show result
+  plt.imshow(originalInput)
+  plt.xlim(224,0)
+  plt.ylim(224,0)
+  plt.title(f'Prediction: {value_predicted} - Actual target: {value_target}')
+  plt.show()
+  
+  torch.save(model, '/content/model.pt')
